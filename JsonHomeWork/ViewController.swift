fdfa49e88125396fe8f119274cfd5f9e13de4bf7
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-04-17 15:00:12 +0300

    Initial Commit

---
+++ JsonHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonHomeWork
+//
+//  Created by Максим Гурков on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
